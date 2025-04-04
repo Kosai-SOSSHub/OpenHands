3127ed0d94bd19f6d242f132efefb005ae714f10
nick-kosai <nick@kosai.com>
2025-04-04 10:50:12 -0500

    Update llm_config.py
    since setting env variables is not setting the retry config values i am changing their default values

--- openhands/core/config/llm_config.py
+++ openhands/core/config/llm_config.py
@@ -56,10 +56,10 @@ class LLMConfig(BaseModel):
     openrouter_site_url: str = Field(default='https://docs.all-hands.dev/')
     openrouter_app_name: str = Field(default='OpenHands')
     # total wait time: 5 + 10 + 20 + 30 = 65 seconds
-    num_retries: int = Field(default=4)
+    num_retries: int = Field(default=8)
     retry_multiplier: float = Field(default=2)
-    retry_min_wait: int = Field(default=5)
-    retry_max_wait: int = Field(default=30)
+    retry_min_wait: int = Field(default=20)
+    retry_max_wait: int = Field(default=640)
     timeout: int | None = Field(default=None)
     max_message_chars: int = Field(
         default=30_000
